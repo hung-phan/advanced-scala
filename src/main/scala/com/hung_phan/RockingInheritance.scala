3927d4911b44ee0ede0ed69e58ab2c199b24eeb1
hungphan <colorvisavn@gmail.com>
2020-06-08 01:33:21 +0800

    Add example for inheritance order

---
+++ src/main/scala/com/hung_phan/RockingInheritance.scala
@@ -0,0 +1,72 @@
+package com.hung_phan
+
+object RockingInheritance extends App {
+  // convenience
+  trait Writer[T] {
+    def write(value: T): Unit
+  }
+  trait Closable {
+    def close(status: Int): Unit
+  }
+  trait GenericStream[T] {
+    // some methods
+    def foreach(f: T => Unit): Unit
+  }
+
+  def processStream[T](stream: GenericStream[T] with Writer[T] with Closable): Unit = {
+    stream.foreach(println)
+    stream.close(0)
+  }
+
+  // diamond problem
+  trait Animal {
+    def name: String
+  }
+  trait Lion extends Animal {
+    override def name: String = "Lion"
+  }
+  trait Tiger extends Animal {
+    override def name: String = "Tiger"
+  }
+  class Mutant extends Lion with Tiger {
+    override def name: String = "Alien"
+  }
+
+  /**
+   * Mutant extends Animal
+   * with { override def name: String = "Lion" }
+   * with { override def name: String = "Tiger" }
+   *
+   * Last override gets picked
+   */
+
+  // the super problem + type linearization
+  trait Cold {
+    def print = println("cold")
+  }
+  trait Green extends Cold {
+    override def print: Unit = {
+      println("green")
+      super.print
+    }
+  }
+  trait Blue extends Cold {
+    override def print: Unit = {
+      println("blue")
+      super.print
+    }
+  }
+  class Red {
+    def print = println("red")
+  }
+  class White extends Red with Green with Blue {
+    override def print: Unit = {
+      println("white")
+      super.print
+    }
+  }
+  val color = new White()
+  color.print
+
+  // white -> blue -> green -> cold
+}
